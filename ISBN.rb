d9a3488acf81599b7130e2bbdff274c87108ce63
JTong <eldertong@gmail.com>
2016-09-28 12:52:12 -0400

    passing for 13 digit numbers

--- ISBN.rb
+++ ISBN.rb
@@ -1,6 +1,8 @@
 def isbn(pro)
-    pro.to_s.split(//).map{|chr| chr.to_i}
+    pro.to_s.split(//).map{|chr| chr.to_i} #take intiger from test and convert it to a string then split the string and map it to an array by character and make each character in the array an intiger
 	if pro.to_s.length == 10
+        'valid'
+    elsif pro.to_s.length == 13
         'valid'
     else
         'invalid'
--- ISBNTest.rb
+++ ISBNTest.rb
@@ -9,7 +9,11 @@ class TestIsbn <Minitest::Test
 	def test_nine_digit_number_results_invalid
 		pro = 123456789
 		assert_equal('invalid', isbn(pro))
-		end
+	end
+	def test_thirteen_digit_number_results_valid
+		pro = 1234567891234
+		assert_equal('valid', isbn(pro))
+	end
 end
 
 #test
