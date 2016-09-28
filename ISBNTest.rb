27fcd341581209d2265e037bbc6da95f7aaaab72
JTong <eldertong@gmail.com>
2016-09-28 12:19:49 -0400

    the night is dark and full of terrors

--- ISBNTest.rb
+++ ISBNTest.rb
@@ -3,8 +3,8 @@ require_relative "isbn.rb"
 
 class TestIsbn <Minitest::Test
 	def test_ten_digit_number_results_valid
-		pro = "1234567891"
-		assert_equal("valid", isbn(pro))
+		pro = 1234567891
+		assert_equal('valid', isbn(pro))
 	end
 end
 
