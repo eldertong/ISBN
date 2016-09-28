2fb7ef6d304c18e1ecad62bf0100e75ca1f8ee88
JTong <eldertong@gmail.com>
2016-09-28 15:58:53 -0400

    fixing while in meeting

--- ISBN.rb
+++ ISBN.rb
@@ -6,15 +6,15 @@ def isbn(pro)
     ar_int = pro.to_s.split(//).map{|chr| chr.to_i}
     #pro.to_s.split(//).map{|chr| chr.to_i} #take intiger from test and convert it to a string then split #the string and map it to an array by character and make each character in the array an intiger
     if pro.to_s.length == 10
-    'valid'
+    'valid length'
     elsif pro.to_s.length == 13
-    'valid'
+    'valid length'
     else
-    'invalid'
+    'invalid length'
     end
     #puts ar_int # ar_int is an array of intigers that i can use to do math for further validation
 end
 
 # pro3 = 1234567891
-
+# look into regex
 # isbn(pro3)
--- ISBNTest.rb
+++ ISBNTest.rb
@@ -4,19 +4,19 @@ require_relative "isbn.rb"
 class TestIsbn <Minitest::Test
 	def test_ten_digit_number_results_valid
 		pro = 1234567891
-		assert_equal('valid', isbn(pro))
+		assert_equal('valid length', isbn(pro))
 	end
 	def test_nine_digit_number_results_invalid
 		pro = 123456789
-		assert_equal('invalid', isbn(pro))
+		assert_equal('invalid length', isbn(pro))
 	end
 	def test_thirteen_digit_number_results_valid
 		pro = 1234567891234
-		assert_equal('valid', isbn(pro))
+		assert_equal('valid length', isbn(pro))
 	end
 	def test_twenty_digit_number_results_invalid
 		pro = 12345678912345678912
-		assert_equal('invalid', isbn(pro))
+		assert_equal('invalid length', isbn(pro))
 	end
 end
 
