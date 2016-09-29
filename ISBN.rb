bfa93368317282c7e0dd329883e5819d327acac1
JTong <eldertong@gmail.com>
2016-09-29 09:43:50 -0400

    trying .delete(' "

--- ISBN.rb
+++ ISBN.rb
@@ -5,9 +5,9 @@
 def isbn(pro)
     ar_int = pro.split(//).map{|chr| chr.to_i}
     #pro.to_s.split(//).map{|chr| chr.to_i} #take intiger from test and convert it to a string then split #the string and map it to an array by character and make each character in the array an intiger
-    if ar_int.length == 10
+    if pro.delete(' ').length == 10
     'valid length'
-    elsif ar_int.length == 13
+    elsif pro.delete(' ').length == 13
     'valid length'
     else
     'invalid length'
