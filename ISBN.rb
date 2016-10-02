32f161369160158dc3b5c5ced440debecf024402
JTong <eldertong@gmail.com>
2016-10-02 07:27:01 -0400

    what am i doing?

--- ISBN.rb
+++ ISBN.rb
@@ -7,8 +7,8 @@ def isbn(pro)
     #pro.to_s.split(//).map{|chr| chr.to_i} #take intiger from test and convert it to a string then split #the string and map it to an array by character and make each character in the array an intiger
     if pro.gsub(/[^0-9,^x,.]/, "").length == 10
         ar_int = pro.split(//).map{|chr| chr.to_i}
-        check_sum = ar_int (([1]*1)+([2]*2)+([3]*3)+([4]*4)+([5]*5)+([6]*6)+([7]*7)+([8]*8)+([9]*9))
-        if check_sum % 11 == ar_int.last
+        check_sum = (ar_int)[([1]*1)+([2]*2)+([3]*3)+([4]*4)+([5]*5)+([6]*6)+([7]*7)+([8]*8)+([9]*9)]
+        if (check_sum.to_i) % 11 == ar_int.last
             "valid"
         else
             "invalid"
