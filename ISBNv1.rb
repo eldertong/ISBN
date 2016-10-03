d28344251c89da5080b25149c3b3de11ee336652
JTong <eldertong@gmail.com>
2016-10-03 17:50:24 -0400

    lots of moving parts here... crossing my fingers

--- ISBNv1.rb
+++ ISBNv1.rb
@@ -18,11 +18,10 @@ def isbn10(pro)
     pro.each.with_index do |value, index|  #this is where the magic happens, we are doing something with the index
         break if index == 9 #this means that when the function will stop looping when it reaches the 9th place
         sum += value * (index + 1) #now every time it runs through the function for the each place it will add the sum up and add one to the index position
-    "valid ten digit isbn"
+    end #ends this portion of the function
+    check10 = sum % 11  #sets checksum to the sum of the sets in the array times their place number divided by 11
+    if check10 == pro[9]  #this compares the checksum and the 10th number in the array, which should be the checksum
+        "valid ten digit isbn"
+    else
+        "invalid ten digit isbn"
 end
-
-
-
-isbn10("0471958697")
-puts pro
-
