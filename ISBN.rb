f891c1b5163dcc968995a43a822b2a2e806e9572
JTong <eldertong@gmail.com>
2016-10-03 07:55:14 -0400

    working yesterday

--- ISBN.rb
+++ ISBN.rb
@@ -7,7 +7,7 @@ def isbn(pro)
     #pro.to_s.split(//).map{|chr| chr.to_i} #take intiger from test and convert it to a string then split #the string and map it to an array by character and make each character in the array an intiger
     if pro.gsub(/[^0-9,^x,.]/, "").length == 10
         ar_int = pro.split(//).map{|chr| chr.to_i}
-        check_sum = (ar_int)[([1.to_i]*1)+([2.to_i]*2)+([3.to_i]*3)+([4.to_i]*4)+([5.to_i]*5)+([6.to_i]*6)+([7.to_i]*7)+([8.to_i]*8)+([9.to_i]*9)]
+        check_sum = (ar_int)([1.to_i]*1)+([2.to_i]*2)+([3.to_i]*3)+([4.to_i]*4)+([5.to_i]*5)+([6.to_i]*6)+([7.to_i]*7)+([8.to_i]*8)+([9.to_i]*9)
         if (check_sum.to_i) % 11 == ar_int.last
             "valid"
         else
