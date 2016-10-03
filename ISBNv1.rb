2af450ee1b503fb41676a49522c13c54477e64a0
JTong <eldertong@gmail.com>
2016-10-03 13:35:11 -0400

    im still cloudy

--- ISBNv1.rb
+++ ISBNv1.rb
@@ -1,9 +1,14 @@
 def isbn(leng)
     if leng.length == 10
         "valid 10"
+
     elsif leng.length == 13
         "valid 13"
     else
         'invalid length'
     end
+end
+
+def isbn10(pro)
+    pro = pro.gsub(/[^0-9,^x,.]/, "")
 end
