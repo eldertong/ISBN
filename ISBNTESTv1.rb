3ec3966b8173a28ad070a88a256c326ed5517219
JTong <eldertong@gmail.com>
2016-10-03 12:26:31 -0400

    working with 10 digit

--- ISBNTESTv1.rb
+++ ISBNTESTv1.rb
@@ -2,7 +2,12 @@ require "minitest/autorun"
 require_relative "isbn.rb"
 
 class TestIsbn <Minitest::Test
-	def test_valid_ten_digit_isbn_returns_true
+	def test_valid_ten_digit_isbn_returns_valid_length
 		length = "0471958697"
 		assert_equal(true, isbn(length))
 	end
+    def test_nine_digit_isbn_returns_valid_length
+		length = "047195869"
+		assert_equal(false, isbn(length))
+	end
+end
---
+++ ISBNv1.rb
@@ -0,0 +1,7 @@
+def isbn(length)
+    if length.length == 10
+       puts  "valid"
+    else
+       puts  "invalid"
+    end
+end
