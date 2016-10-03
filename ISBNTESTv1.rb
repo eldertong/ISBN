10aee48c175e03c0e157285eac3855f0dc091639
JTong <eldertong@gmail.com>
2016-10-03 13:36:07 -0400

    add dash to test

--- ISBNTESTv1.rb
+++ ISBNTESTv1.rb
@@ -14,4 +14,8 @@ class TestIsbn <Minitest::Test
 		leng = "9780470059029"
 		assert_equal('valid 13', isbn(leng))
 	end
+	def test_valid_ten_digit_isbn_returns_valid_length
+		leng = "04-719-58697"
+		assert_equal('valid 10', isbn(leng))
+    end
 end
