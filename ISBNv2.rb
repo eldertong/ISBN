00dac520089f7838f89d39e9fe234696dd332a14
JTong <eldertong@gmail.com>
2016-10-04 14:12:26 -0400

    added delete option from ysa

--- ISBNv2.rb
+++ ISBNv2.rb
@@ -1,4 +1,7 @@
 def valid_isbn?(isbn)
-    isbn.gsub!(" ", "") || isbn.gsub!("-", "")#this strips the whitespace and redifines isbn by using the !. remember ! is permenantish
+    isbn.gsub!(" ", "") #this strips the whitespace and redifines isbn by using the !. remember ! is permenantish
+    isbn.gsub!("-", "") #this strips the dashes from isbn and redefines it without them
+    # isbn.delete!(" ") #this deletes the item in the quotes
+    # isbn.delete!("-")
     isbn.length == 10 #same as doing and if else statement
 end
