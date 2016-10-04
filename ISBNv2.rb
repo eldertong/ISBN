0baa5358f329092d92290d65c22b726d112b6d5d
JTong <eldertong@gmail.com>
2016-10-04 14:20:06 -0400

    i am jacob's failures

--- ISBNv2.rb
+++ ISBNv2.rb
@@ -1,7 +1,15 @@
 def valid_isbn?(isbn)
-    isbn.gsub!(" ", "") #this strips the whitespace and redifines isbn by using the !. remember ! is permenantish
-    isbn.gsub!("-", "") #this strips the dashes from isbn and redefines it without them
-    # isbn.delete!(" ") #this deletes the item in the quotes
-    # isbn.delete!("-")
+    isbn = remove_spaces(isbn)
+    isbn = remove_dashes(isbn)
     isbn.length == 10 #same as doing and if else statement
+end
+
+def remove_spaces(isbn)
+    isbn.gsub(" ", "") #this strips the whitespace and redifines isbn by using the !. remember ! is permenantish
+    # isbn.delete!(" ") #this deletes the item in the quotes
+end
+
+def remove_dashes(isbn)
+    isbn.gsub("-", "") #this strips the dashes from isbn and redefines it without them
+    # isbn.delete!("-")
 end
