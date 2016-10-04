3a365520a3d01accf1e145d0cfb8f517c6ea7978
JTong <eldertong@gmail.com>
2016-10-04 15:33:47 -0400

    hard to go slow and small

--- ISBNv2.rb
+++ ISBNv2.rb
@@ -1,10 +1,18 @@
 def valid_isbn?(isbn)
     isbn = remove_spaces(isbn) #calls the function referenced and redefines isbn
     isbn = remove_dashes(isbn)
-    isbn.length == 10 #same as doing and if else statement
+    if isbn.length == 10 && isbn[9] == "7" #same as doing and if else statement
+        true
+    else
+        false
+    end
     #multiply each digit in the string by it's position for the first 9 digits, then add them and "modulo" cuz thats not a real word! by 11, this should equal the last digit
-    sum = 0
-    isbn.chars.map!(&:to_i) #splits the string into an array of individual characters as intigers
+    # ten_digit_array = isbn.chars.map!(&:to_i) #splits the string into an array of individual characters as intigers & is shorthand for something longer - research this later
+    # sum = 0
+    # ten_digit_array.each_with_index do |value, index|
+    #     break if index == 9
+    #     sum += (index + 1) * value
+    # end
 end
 
 def remove_spaces(isbn)
