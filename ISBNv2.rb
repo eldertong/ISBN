e0a48e86a77729ed233e1a66158bd67c7e1179dc
JTong <eldertong@gmail.com>
2016-10-04 14:09:53 -0400

    trying to shorten things

--- ISBNv2.rb
+++ ISBNv2.rb
@@ -1,4 +1,4 @@
 def valid_isbn?(isbn)
-    isbn.gsub!(" ", "") #this strips the whitespace and redifines isbn by using the ! remember ! is permenantish
+    isbn.gsub!(" ", "") || isbn.gsub!("-", "")#this strips the whitespace and redifines isbn by using the !. remember ! is permenantish
     isbn.length == 10 #same as doing and if else statement
 end
