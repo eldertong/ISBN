38634f3fb62c870c0ff05771b8147252c358e8bb
JTong <eldertong@gmail.com>
2016-10-04 14:52:32 -0400

    susan be a warlock

--- ISBNv2.rb
+++ ISBNv2.rb
@@ -2,6 +2,9 @@ def valid_isbn?(isbn)
     isbn = remove_spaces(isbn) #calls the function referenced and redefines isbn
     isbn = remove_dashes(isbn)
     isbn.length == 10 #same as doing and if else statement
+    #multiply each digit in the string by it's position for the first 9 digits, then add them and "modulo" cuz thats not a real word! by 11, this should equal the last digit
+    sum = 0
+    isbn.chars.map!(&:to_i) #splits the string into an array of individual characters as intigers
 end
 
 def remove_spaces(isbn)
