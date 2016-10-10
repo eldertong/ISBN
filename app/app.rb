76cfa9ad9a09dc88c0fc2848ea25338e88310415
JTong <eldertong@gmail.com>
2016-10-10 17:51:17 -0400

    updated

--- app/app.rb
+++ app/app.rb
@@ -2,14 +2,28 @@ require 'sinatra'
 require_relative "isbnv2.rb"
 #require any gems
 get '/' do 
-	erb :whats_your_name
+	erb :welcome
 end
 
 post '/name' do
 	name = params[:input_name]
 	redirect '/age?input_name=' + name
+end
+
+get '/fav_nums' do
+	age = params[:age]
+	name = params[:input_name]
+
+	erb :fav_nums, :locals => {:name => name, :age => age}
 
 end
+
+post '/fav_nums' do
+	age = params[:age]
+	name = params[:input_name]
+	favnum1 = params[:num1]
+	favnum2 = params[:num2]
+	favnum3 = params[:num3]
 
 get '/contact' do
 	erb :contact
